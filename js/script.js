f0625922b4902ffa8dc1ecd427a361a3e3c929ab
Hamidreza Jafari <101067643+hamid-reza-jafari@users.noreply.github.com>
2022-04-10 10:11:38 +0430

    // First

---
+++ js/script.js
@@ -0,0 +1,26 @@
+﻿ // Functions for select elems with id & class
+ var $ = document;
+ function _id(id_name) {
+   return $.getElementById(id_name);
+ }
+
+ function _class(class_name) {
+   return $.getElementsByClassName(class_name);
+ }
+
+ // Select Elems
+
+ var togglePassword = $.querySelector(".togglepassword");
+ var passwordField = _id("passwordfield");
+
+ // Fire click event on eye icon
+ togglePassword.addEventListener("click", function () {
+
+   if (passwordField.type == "text") {
+     passwordField.type = "password";
+     togglePassword.classList.add("active");
+   } else {
+     passwordField.type = "text";
+     togglePassword.classList.remove("active");
+   }
+ })
